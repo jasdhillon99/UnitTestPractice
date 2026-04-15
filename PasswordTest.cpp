22851fe99624b40d11b8fb77481080db9ac04ff5
jas <jdhillon5@csuchico.edu>
2026-04-15 10:32:34 -0700

    monday 1 work and cleanup

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -1,22 +1,42 @@
 /**
  * Unit Tests for Password class
-**/
+ **/
 
-#include <gtest/gtest.h>
 #include "Password.h"
+#include <gtest/gtest.h>
 
-class PasswordTest : public ::testing::Test
-{
-	protected:
-		PasswordTest(){} //constructor runs before each test
-		virtual ~PasswordTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor)
+class PasswordTest : public ::testing::Test {
+protected:
+  PasswordTest() {}          // constructor runs before each test
+  virtual ~PasswordTest() {} // destructor cleans up after tests
+  virtual void SetUp() {}    // sets up before each test (after constructor)
+  virtual void TearDown() {} // clean up after each test, (before destructor)
 };
 
-TEST(PasswordTest, single_letter_password)
-{
-	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+TEST(PasswordTest, single_letter_password) {
+  Password my_password;
+  int actual = my_password.count_leading_characters("Z");
+  ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, mixed_case_password) {
+  Password my_password;
+  int actual = my_password.count_leading_characters("ZZz");
+  ASSERT_EQ(2, actual);
+}
+TEST(PasswordTest, empty) {
+  Password my_password;
+  int actual = my_password.count_leading_characters("");
+  ASSERT_EQ(0, actual);
+}
+TEST(PasswordTest, spaces) {
+  Password my_password;
+  int actual = my_password.count_leading_characters("   ");
+  ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, space_before_pass) {
+  Password my_password;
+  int actual = my_password.count_leading_characters(" ZZ");
+  ASSERT_EQ(2, actual);
 }
